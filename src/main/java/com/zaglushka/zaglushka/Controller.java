2e4d72d6d05c33a8433709d17b97ce68622855d3
Danya <117893294+hekkiii@users.noreply.github.com>
2023-08-17 17:45:38 +0300

    Version 1.1.2
    Changed mapping for getting() in Controller to GET only

--- src/main/java/com/zaglushka/zaglushka/Controller.java
+++ src/main/java/com/zaglushka/zaglushka/Controller.java
@@ -1,12 +1,13 @@
 package com.zaglushka.zaglushka;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping(value = "/getting", method = RequestMethod.GET)
+    @GetMapping(value = "/getting")
     public Getting getting(){
         return new Getting();
     }
