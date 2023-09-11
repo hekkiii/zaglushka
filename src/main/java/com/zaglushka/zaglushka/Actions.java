6bd55251cd9d094d812a6aedaa423cd6a24a3648
Danya <117893294+hekkiii@users.noreply.github.com>
2023-09-11 12:58:56 +0300

    Version 3.0.1c
    Changed db to localhost

--- src/main/java/com/zaglushka/zaglushka/Actions.java
+++ src/main/java/com/zaglushka/zaglushka/Actions.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Actions {
-    private static final String URL = "jdbc:postgresql://localhost:5432/testdb";
+    private static final String URL = "jdbc:postgresql://postgres:5432/testdb";
     private static final String dbUser = "postgres";
     private static final String dbPass = "test";
     private static final String table1 = "first";
